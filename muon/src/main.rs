72226716059971472ed0983d01359739d08b0409
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2020-07-05 18:47:31 +0100

    Refactor main function

--- muon/src/main.rs
+++ muon/src/main.rs
@@ -6,43 +6,28 @@ use bitcoin::messages::{Options, Serializable};
 #[tokio::main]
 async fn main() {
     let mut stream = TcpStream::connect("seed.bitcoinstats.com:8333").await.unwrap();
-    println!("created Bitcoin stream");
 
+    // Build Version message
+    let opt: Options = Options{
+        version: bitcoin::messages::PROTOCOL_VERSION,
+        is_version_message: true,
+    };
     let version: bitcoin::messages::Version = bitcoin::messages::Version::new(
-        bitcoin::messages::Address::new(
-            stream.local_addr().unwrap().ip(),
-        ),
-        bitcoin::messages::Address::new(
-            stream.peer_addr().unwrap().ip(),
-        ),
+        bitcoin::messages::Address::new(stream.local_addr().unwrap().ip()),
+        bitcoin::messages::Address::new(stream.peer_addr().unwrap().ip()),
         1,
     );
+    let version_msg = bitcoin::messages::Message::new(bitcoin::messages::Network::MAINNET, &version, &opt);
 
-    println!("Building message");
-    let version_msg = bitcoin::messages::Message::new(
-        bitcoin::messages::Network::MAINNET,
-        &version,
-        &Options {
-            version: bitcoin::messages::PROTOCOL_VERSION,
-            is_version_message: true,
-        },
-    );
-
-    println!("Serializing message");
+    // Serialize Version message
     let mut msg: Vec<u8> = Vec::new();
-    version_msg.to_wire(
-        &mut msg,
-        &Options {
-            version: bitcoin::messages::PROTOCOL_VERSION,
-            is_version_message: true,
-        }
-    );
+    version_msg.to_wire(&mut msg, &opt);
 
-    println!("Sending message");
+    // Send Version message
     stream.write(&msg).await.unwrap();
     println!("Sent version {:x?}", msg.as_slice());
 
-    println!("Sending verack");
+    // Read Verack message
     let mut ack_version: Vec<u8> = Vec::new();
     let _ = stream.read(&mut ack_version).await;
     println!("{:x?}", ack_version.as_slice());
